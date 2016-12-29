1adb28303da34176d5a3f67c2d7d064e88a0acff
amsyar <amsyar@sigaint.org>
2016-12-29 21:23:33 +0800

    age checker fix.

--- 1/Hi Human.py
+++ 1/Hi Human.py
@@ -15,7 +15,12 @@ while not r.match(human_name):                         # prevent Human from typi
 print('Nice to meet you ' + human_name)
 print('How old are you ' + human_name + ' ?')   # asking for human age
 
-human_age = int(input('your age : '))   # human type the age
+while True:
+    try:
+        human_age = int(input('your age : '))   # human type the age
+        break
+    except ValueError:
+        print('I said age ' + human_name + ', not words')
 
 while human_age > 117:
         print('I know I ask you to type your age, but you must not be that old right')
