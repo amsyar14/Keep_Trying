c96ac3f8dffb458e0ac5c6d9e1cb2b6776b5c292
amsyar <amsyar@sigaint.org>
2016-12-29 18:34:56 +0800

    add age checker.

--- 1/Hi Human.py
+++ 1/Hi Human.py
@@ -1,24 +1,27 @@
 print('Hello Human')                          # just a greet
 print('My name is EZ, What yours human ?')     # asking for human name
 
-human_name = input()   # human type the name
+human_name = input('Your Name : ')   # human type the name
 
 #while human_name != str():                             # prevent Human from typing numbers
 #    print('Name only have words Human, not numbers')   #
 #    print('please type your name Human')               #
-#    human_name = input()                               #
+#    human_name = input('Your Name:')                               #
 
 print('Nice to meet you ' + human_name)
 print('How old are you ' + human_name + ' ?')   # asking for human age
 
-human_age = input()   # human type the age
+human_age = int(input('your age : '))   # human type the age
+while human_age > 117:
+    print('I know I ask you to type your age, but you must not be that old right')
+    human_age = input('please type your real age : ')
+    continue
 
-if human_age <= '12':
+if human_age <= 12:
     print('you are still a child I see')   # check age
-    print('you are still a child I see')   # check age
-elif human_age <= '18':
+elif human_age <= 18:
     print('you are a teenager I see')
-elif human_age > '18':
+elif human_age > 18:
     print('you are adult I see')
 
 #print()
